a29779a537a794b784a8b071d234e676e9aed259
aakash sirohi <aakashsrh@gmail.com>
2023-02-16 03:10:30 +0530

    worked on csrp

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 var mysql = require('mysql2');
+const csurf = require('csurf');
 
 const app = express();
-
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(csurf({ cookie: true }));
 
-
-const conn =mysql.createConnection({
+const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'aakash',
     database: 'auth'
 });
 
-
-
 conn.connect((err)=> {
     if(err) throw err   ;
     console.log('Mysql connected...');
 });
 app.use(bodyParser.json());
+app.get('/csrf' ,(req,res)=> {
+    res.json({ csrfToken: req.csrfToken() });
+    
+});
 
 //Registering a user using POST
 app.post('/register' , (req,res)=>{
@@ -44,43 +49,64 @@ app.post('/register' , (req,res)=>{
     });
 
 
-//Getting users using GET 
-app.get('/register', function(req,res){
-    res.send('Get was called');
-})    
+// //Getting users using GET 
+// app.get('/register', function(req,res){
+//     res.send('Get was called');
+// })    
 
 app.post('/login', function(req,res){
+console.log(req.body.data.csrfToken );
+    if (req.csrfToken() !== req.body.data.csrfToken) {
+        res.status(403).send('CSRF token invalid');
+        return;
+      }
     
     const username = req.body.data.username;
     const password = req.body.data.password;
     const checkUserSql = 'SELECT * FROM users where username = ?';
     const passwordMatch = 'SELECT password FROM users WHERE username=?';
+
     conn.query(checkUserSql,[username],(error,results) => {
         if(error){
             throw error;
         }
-        if(results.length>0){
-            console.log('user with this username exists in DB');
-            console.log('username entered by the user is: ',username + 'password entered by the user is: ',password);
-            conn.query(passwordMatch,[username],(error,results)=>{
-                if(error){
-                    throw error;
-                } if(results){
-                    res.json({message:'User exists and passwords are matching',status:200});
-                }else{
-                    res.json({message:'User exists but passwords do not match',status:400});
-                }
-            })
-        }else{
-            console.log('user with this username does not exist in DB');
-            res.json({message:'User Does Not Exist',status:404});
-            
-        }
-        
-    })
+        // if(results.length>0){
+        //     results.forEach((row) => {
+        //         console.log('User:', row)});
+        // }else{
+        //     console.log('user with this username does not exist in DB');
+        //     res.json({message:'User Does Not Exist',status:404});
+        // }
+    });
 
+    conn.query(passwordMatch,[username],(error,results)=>{
+        let passdb;
+        if(error){
+            throw error;
+        } 
+        if(results.length>0){
+            results.forEach((row) => {
+                passdb = row.password;
+                console.log(passdb);
+            });}
+                
+        if(passdb==password){
+            res.json({message:'Success',status:200,csrfToken: req.csrfToken()});
+        }else{
+            res.json({message:'Incorrect Password',status:400});
+        }
+               
     
+
+});
+   
+
+
 })
+
+
+
+
 app.listen(4000,()=> {
   console.log("Server running successfully on 4000");
 
